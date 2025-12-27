efa9691c3a718269b29c3c1d19c2af3d8ff9094d
steath094 <armandawoodkhan@gmail.com>
2025-12-27 22:51:11 +0530

    feat: Implement initial web application with an Express backend for user management and a Next.js frontend to display users.

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { connectDB, User } from '@repo/db';
 dotenv.config()
 await connectDB();
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
@@ -21,6 +23,10 @@ app.post("/signup", async (req, res) => {
         console.log(e)
         res.status(500).json({ error: "Failed to create user" })
     }
+})
+app.get("/users", async (req, res) => {
+    const users = await User.find()
+    res.json(users)
 })
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
